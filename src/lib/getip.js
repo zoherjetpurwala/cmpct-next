a41995ac237bce32fb0c3ba4090bb88adc929938
zoherjetpurwala <zoher.jetpurwala@gmail.com>
2024-11-29 14:20:24 +0530

    Update getip.js

--- src/lib/getip.js
+++ src/lib/getip.js
@@ -1,11 +1,13 @@
 export function getClientIp(req) {
-    const ip = 
-      req.ip ||
-      req.headers.get('x-real-client-ip') ||
-      req.headers.get('x-forwarded-for')?.split(',')[0] ||
-      req.headers.get('x-real-ip') ||
-      req.headers.get('cf-connecting-ip') ||
-      '127.0.0.1'
-  
-    return ip.replace(/^::ffff:/, '')
-  }
+  const ip =
+    req.ip ||
+    req.headers.get("x-real-client-ip") ||
+    req.headers.get("x-forwarded-for")?.split(",")[0] ||
+    req.headers.get("x-real-ip") ||
+    req.headers.get("cf-connecting-ip") ||
+    "127.0.0.1";
+    
+  const cleanIp = ip.replace(/^::ffff:/, "");
+
+  return cleanIp;
+}
